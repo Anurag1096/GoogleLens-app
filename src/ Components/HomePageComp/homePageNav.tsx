58a4dc269b132d98b3ac66b3c83fc7d2f8f34b55
Anurag1096 <anuragqwerty09@outlook.com>
2025-04-14 19:32:02 +0530

    wip: Adding nav icons to the nav bar in home page

--- src/ Components/HomePageComp/homePageNav.tsx
+++ src/ Components/HomePageComp/homePageNav.tsx
@@ -1,8 +1,25 @@
 
-
+import './homePageNav.css'
 function HomePageNav() {
+
+  const initialServer=null;
+
+  const handleLabClick=()=>{
+    alert("clicked lab icon")
+  }
+
+  const handleInitialClick=()=>{
+    alert('now showing the popover');
+  }
   return (
-    <div>homePageNav</div>
+    <div className="nav-wrapper">
+     <div className="btn-labs" onClick={handleLabClick}>
+       <img src="/vite.svg" alt='labs_img' width={34} height={34}/>
+     </div>
+     <div  className="btn-initial" onClick={handleInitialClick}>
+       <span>{`${initialServer??"A"}`}</span>
+     </div>
+    </div>
   )
 }
 
--- src/App.tsx
+++ src/App.tsx
@@ -3,12 +3,16 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 import CameraComponent from './ Components/camera'
+import HomePage from './Pages/HomePage/HomePage'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
+    <div>
+      <HomePage/>
+    </div>
     <div>
       <CameraComponent/>
     </div>
