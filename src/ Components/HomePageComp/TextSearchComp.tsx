33e0d1d1f1ff3c5ab863449ce4c2fc174acc9e49
Anurag1096 <anuragqwerty09@outlook.com>
2025-04-15 20:53:13 +0530

    wip:creating text search component

--- src/ Components/HomePageComp/TextSearchComp.tsx
+++ src/ Components/HomePageComp/TextSearchComp.tsx
@@ -1,8 +1,49 @@
+import { useState } from 'react'
+import './textSearchComp.css'
 
-
+// this thing will be a page
 function TextSearchComp() {
+  const [searchVal, setSearchVal] = useState<string>("")
+  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setSearchVal(e.target.value)
+  }
   return (
-    <div>TextSearchComp</div>
+    <div className='input-wrapper'>
+      <span className="input-icon left" onClick={() => alert("this will close the modal")}>
+         {/* You can use an SVG or Font Awesome here */}
+        🔙
+      </span>
+
+      <input
+        type='text'
+        className="text-search"
+        value={searchVal}
+        onChange={handleChange}
+        placeholder="Search something..."
+        autoFocus
+        aria-label="Search input"
+      />
+      {/* if search val is there then a x icon will be displayed */}
+      {searchVal && (
+        <span className="input-icon right" onClick={() => setSearchVal("")}>
+          ❌
+        </span>
+      )}
+      {/* When there is no search tow icons will be displayed */}
+      {!searchVal && (
+        <div>
+          <span className="input-icon right" onClick={() => setSearchVal("")}>
+          🎤
+          </span>
+          <span className="input-icon right2" onClick={() => setSearchVal("")}>
+          📷
+          </span>
+        </div>
+      )}
+
+    </div>
+
+
   )
 }
 
